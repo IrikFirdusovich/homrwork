ee396b71e47b8e6ab64e3f5d2b43c9342179e4e4
IrikFirdusovich <Sattarov_i_f@mail.ru>
2024-08-22 22:28:25 +0300

    20/08

---
+++ module_2_3.py
@@ -0,0 +1,13 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+n = 0
+zero = 0
+while n < len(my_list):
+    if my_list[n] > 0:
+        print(my_list[n])
+        n += 1
+    elif my_list[n] == 0:
+        break
+    elif my_list[n] > 0:
+        print(my_list[n])
+        n += 1
+print('список завешен')
